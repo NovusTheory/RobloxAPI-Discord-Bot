4d7b006a0712860578bf1bde4b9af44cfef2471a
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2025-06-11 22:03:58 -0500

    feat!: Total rewrite of the bot with Luau utilizing Meilisearch

--- bot/elasticsearch/init.lua
+++
@@ -1,3 +0,0 @@
-return {
-    Client = require("./client")
-}
--- consts.lua
+++
@@ -1,4 +0,0 @@
-return {
-    ROBLOX_CDN_SETUP_URL = "https://setup.rbxcdn.com",
-    ROBLOX_DEV_HUB_URL = "https://developer.roblox.com"
-}
